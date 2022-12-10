90de3687cc1e3211a90ada2ef1d1e99d82d2a99f
Stanislav Gutsev <gutsstas@mail.ru>
2022-12-10 11:58:09 +0100

    feat: add interfaces for project

---
+++ src/interface.ts
@@ -0,0 +1,37 @@
+export interface SourceInt {
+  category?: string;
+  country?: string;
+  description?: string;
+  id: string;
+  language?: string
+  name: string;
+  url?: string;
+}
+
+export interface Start {
+  articles: Article[];
+  status: string;
+  totalResults: number;
+}
+
+export interface Article {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: SourceInt;
+  title: string;
+  url: string;
+  urlToImage: string;
+}
+
+export interface DrawNews {
+  articles: Article[];
+  status: string;
+  totalResults: number;
+}
+
+export interface DrawSources {
+  sources: SourceInt[];
+  status: string;
+}
